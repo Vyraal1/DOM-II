c8207f011b2cf3182c8ea383766398a67b804c63
Vyraal1 <kevin.afable@gmail.com>
2019-09-03 19:30:22 -0400

    some styling

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// Your code goes here
+// Events
+const body = document.querySelector("body");
+const logoHeading = document.querySelector("logo-heading");
+
+const introImg = document.querySelector(".intro img");
+introImg.addEventListener(`wheel`, e => {
+  event.preventDefault();
+});
+
+//`mouseover`
+const mainImages = document.querySelectorAll(".img-content img");
+console.log(mainImages);
+
+//`keydown``drag / drop``load``focus``resize`
+//
+//
+
+const destination = document.querySelector(".destination");
+//destination.addEventListener(`scroll`, e => {
+destination.addEventListener(`click`, e => {
+  body.style.color = "yellow";
+  body.style.backgroundColor = "red";
+});
+
+//`select`;
+
+// clicked twice on an element
+//`dblclick`;
