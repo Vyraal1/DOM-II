6ddef44cec34622010cbd4be792c57c444a4829c
Vyraal1 <kevin.afable@gmail.com>
2019-09-04 16:35:14 -0400

    10 random event listeners to meet MVP

--- js/index.js
+++ js/index.js
@@ -1,28 +1,66 @@
 // Events
+const textInput = document.createElement("form");
 const body = document.querySelector("body");
-const logoHeading = document.querySelector("logo-heading");
 
 const introImg = document.querySelector(".intro img");
 introImg.addEventListener(`wheel`, e => {
   event.preventDefault();
 });
 
-//`mouseover`
+//
 const mainImages = document.querySelectorAll(".img-content img");
-console.log(mainImages);
+mainImages.forEach(img => {
+  console.log(img);
+  img.addEventListener("mouseover", e => {
+    event.preventDefault();
+  });
+});
 
-//`keydown``drag / drop``load``focus``resize`
-//
-//
+// `keydown` - You're a blogger now
+const logoHeading = document.querySelector(".logo-heading");
+window.addEventListener("keydown", e => {
+  logoHeading.textContent += event.key;
+});
 
-const destination = document.querySelector(".destination");
+// `drag / drop`
+
+// `load`
+window.addEventListener("load", e => {
+  alert("Did you know that oreos are vegan?");
+});
+
+// `focus`
+
+// `resize`
+
+const destinations = document.querySelectorAll(".destination");
 //destination.addEventListener(`scroll`, e => {
-destination.addEventListener(`click`, e => {
-  body.style.color = "yellow";
-  body.style.backgroundColor = "red";
+destinations.forEach(dest => {
+  dest.addEventListener(`click`, e => {
+    body.style.color = "yellow";
+    body.style.backgroundColor = "red";
+  });
+});
+
+destinations.forEach(dest => {
+  dest.addEventListener(`mouseover`, e => {
+    dest.style.backgroundColor = "blue";
+  });
+  dest.addEventListener("mouseleave", e => {
+    dest.style.backgroundColor = "white";
+  });
 });
 
 //`select`;
 
-// clicked twice on an element
 //`dblclick`;
+const ps = document.querySelectorAll("p");
+ps.forEach(p =>
+  p.addEventListener("dblclick", e => {
+    p.style.color = body.style.color;
+  })
+);
+
+// Questions
+// * what are the most common ones we use in react?
+// * how do you get the P elements to match the body color?
